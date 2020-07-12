91bee5f8372d4dc432896167eb32e4631b1d7ed2
JILLBAI <tiger90483@gmail.com>
2020-07-12 15:02:55 +0800

    yu

---
+++ main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function()
+{
+    $("#circleslider3").tinycircleslider({
+        dotsSnap : true
+    ,   radius   : 170
+    ,   dotsHide : false
+    });
+
+    $("#circleslider3").find("a").fancybox();
+
+});
+   

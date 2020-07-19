02dd7c706f8258e3718a77bccffce019a1cfe2ee
JILLBAI <tiger90483@gmail.com>
2020-07-19 12:40:22 +0800

    R

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
 
-$(document).ready(function()
-{
-    $("#circleslider3").tinycircleslider({
-        dotsSnap : true
-    ,   radius   : 170
-    ,   dotsHide : false
-    });
 
-    $("#circleslider3").find("a").fancybox();
-
-});
    
